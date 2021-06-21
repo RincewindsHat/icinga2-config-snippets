a7aec57721fa16f6665eda8a5e4ac21fa229cce1
Lorenz Kästle <lorenz.kaestle@netways.de>
2021-06-21 18:59:00 +0200

    Add script for doing dirty things

---
+++ deployment/misc/icing2_sign_all_certificates.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+
+# Exit on error
+set -e
+
+# Debugging
+#set -x
+
+FINGERPRINTS=$(icinga2 ca list | tail -n +3 | awk '{print $1}')
+
+while read fingerprint; do
+        echo "Signing fingerprint: $fingerprint"
+        icinga2 ca sign $fingerprint
+done <<< $FINGERPRINTS
